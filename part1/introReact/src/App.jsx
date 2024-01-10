b4196e1ed4e86c19ebab1f73467b459ff8abcdbb
Nolan Rangel <nrangel@sudscreative.com>
2024-01-09 18:47:07 -0700

    1.1 & 1.2 completed

--- part1/introReact/src/App.jsx
+++ part1/introReact/src/App.jsx
@@ -30,11 +30,11 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
-     return (
-         <>
-             <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-         </>
-     )
+    return (
+        <>
+            <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+        </>
+    )
 }
 
 const App = () => {
