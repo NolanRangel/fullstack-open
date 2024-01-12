b056a010f931020b98017e322fda700f51b82cff
Nolan Rangel <nrangel@sudscreative.com>
2024-01-12 13:07:32 -0700

    part1 1.13 anecdotes step2 complete

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,6 +1,50 @@
 import { useState } from 'react'
 
+
+const Anecdote = ({ anecdotes, selected, points }) => {
+
+    return (
+        <div>
+            <h2>
+                {anecdotes[selected]}
+            </h2>
+            <p>
+                has {points[selected]} votes
+            </p>
+        </div>
+    )
+}
+
+const Button = ({ text, randomAnecdote, handleVote }) => {
+
+    return (
+        <>
+            { text === "next anecdote" ?
+                <button onClick={() => randomAnecdote()}>
+                    {text}
+                </button>
+            :
+                <button onClick={() => handleVote()}>
+                    {text}
+                </button>
+            }
+
+        </>
+    )
+}
+
 const App = () => {
+    const [selected, setSelected] = useState(0)
+    const [points, setPoints] = useState({
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0,
+        7: 0,
+    })
     const anecdotes = [
         'If it hurts, do it more often.',
         'Adding manpower to a late software project makes it later!',
@@ -12,11 +56,38 @@ const App = () => {
         'The only way to go fast, is to go well.'
     ]
 
-    const [selected, setSelected] = useState(0)
+
+    const randomAnecdote = () => {
+        let anecdoteLength = anecdotes.length;
+
+        setSelected(0)
+        setSelected(Math.floor(Math.random() * anecdoteLength))
+
+
+        const newPoints = {
+            ...points,
+            [selected]: points[selected] += 1
+        }
+        setPoints(newPoints);
+
+
+    };
+
+    const handleVote = () => {
+        const newPoints = {
+            ...points,
+            [selected]: points[selected] += 1
+        }
+        setPoints(newPoints);
+    }
+
+    console.log(points)
 
     return (
         <div>
-            {anecdotes[selected]}
+            <Anecdote anecdotes={anecdotes} selected={selected} points={points}/>
+            <Button text={"vote"} handleVote={handleVote}/>
+            <Button text={"next anecdote"} randomAnecdote={randomAnecdote}/>
         </div>
     )
 }
