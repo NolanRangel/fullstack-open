15a8b847f25771cd9f46b54b0e7287ab3e12a6e3
Nolan Rangel <nrangel@sudscreative.com>
2024-01-25 13:50:05 -0700

    part3 3.6 step 6 complete

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,16 +2,17 @@ const express = require('express');
 const app = express();
 
 
+// Register request timestamp
 const timestamp = function (req, res, next) {
     req.timestamp = new Date
 
     next()
 }
-
 app.use(timestamp)
+app.use(express.json())
 
 
-const persons = [
+let persons = [
     {
         "id": 1,
         "name": "Arto Hellas",
@@ -34,15 +35,67 @@ const persons = [
     }
 ]
 
-
+// GET all
 app.get('/api/persons', (req, res) => {
     res.send(persons);
 })
 
+// GET one by id
+app.get('/api/person/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const person = persons.find(person => person.id === id);
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+// CREATE phonebook entry
+app.post('/api/persons', (req, res) => {
+    const person = req.body;
+
+    if (!person.name) {
+        return res.status(400).json({
+            error: 'Name cannot be empty'
+        })
+    }
+    else if (!person.number) {
+        return res.status(400).json({
+            error: 'Phone number cannot be empty'
+        })
+    }
+    else {
+        const personId = Math.floor(Math.random() * 1000000000)
+        person.id = personId;
+        const personExists = persons.find(p => p.name === person.name)
+
+        if (personExists) {
+            return res.status(400).json({
+                error: 'Name must be unique'
+            })
+        }
+        else {
+            persons = persons.concat(person);
+            res.json(person)
+        }
+    }
+})
+
+// DELETE phonebook entry
+app.delete('/api/person/:id', (req, res) => {
+    const id = Number(req.params.id);
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
+})
+
+// Show phonebook length & Request timestamp
 app.get('/info', (req, res) => {
     let phonebookInfo = `Phonebook has info for ${persons.length} people<br><br><p>Requested at: ${req.timestamp}</p>`
 
-    res.send(phonebookInfo)
+    res.json(phonebookInfo)
 })
 
 
