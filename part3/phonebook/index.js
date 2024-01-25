5ba97883ce8b941fbae98292029a7fe3f92d444e
Nolan Rangel <nrangel@sudscreative.com>
2024-01-24 19:02:00 -0700

    part3 3.1 & 3.2 completed

---
+++ part3/phonebook/index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+
+
+const timestamp = function (req, res, next) {
+    req.timestamp = new Date
+
+    next()
+}
+
+app.use(timestamp)
+
+
+const persons = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+
+
+app.get('/api/persons', (req, res) => {
+    res.send(persons);
+})
+
+app.get('/info', (req, res) => {
+    let phonebookInfo = `Phonebook has info for ${persons.length} people<br><br><p>Requested at: ${req.timestamp}</p>`
+
+    res.send(phonebookInfo)
+})
+
+
+
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
