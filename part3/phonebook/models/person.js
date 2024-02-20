074bf4ff2e7d36d283f1f2ec320d9fdaf31b1242
Nolan Rangel <nrangel@sudscreative.com>
2024-02-19 18:40:05 -0700

    3.22 done. part 3 completegit status

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -1,38 +1,38 @@
-const mongoose = require('mongoose').default;
+const mongoose = require('mongoose').default
 mongoose.set('strictQuery', false)
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch(error => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        minLength: 8,
-        match:  /^\d{2,3}-\d{6,}$/ || /^\d{8,}$/,
-        required: true
-    }
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    match:  /^\d{2,3}-\d{6,}$/ || /^\d{8,}$/,
+    required: true
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -1,10 +1,10 @@
 // const mongoose = require('mongoose')
 // Needed for Webstorm
-const mongoose = require('mongoose').default;
+const mongoose = require('mongoose').default
 
 if (process.argv.length<3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -18,29 +18,29 @@ mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+  name: String,
+  number: String
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 const person = new Person({
-    name: name,
-    number: number
+  name: name,
+  number: number
 })
 
 if (!name || !number) {
-    Person.find({}).then(result => {
-        result.forEach(person => {
-            console.log(person)
-            mongoose.connection.close()
-        })
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person)
+      mongoose.connection.close()
     })
+  })
 } else {
-    person.save().then(result => {
-        console.log('Person saved!')
-        mongoose.connection.close()
-    })
+  person.save().then(result => {
+    console.log('Person saved!')
+    mongoose.connection.close()
+  })
 }
 
 
