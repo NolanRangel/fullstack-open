b708fb66776bb2f713ee4539e378a2436f7b2d21
Nolan Rangel <nrangel@sudscreative.com>
2024-02-26 19:23:31 -0700

    4.2 completed DB crashing on Mongo side

---
+++ part4/blog/models/blog.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose').default
+require('dotenv').config()
+const mongoUrl = process.env.MONGODB_URI
+
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+
+
+// mongoose.set('strictQuery',false)
+// mongoose.connect(mongoUrl)
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+
+module.exports = mongoose.model('Blog', blogSchema)
