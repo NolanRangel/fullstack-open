e236b957b7d157e28d4334ad3d835d9d5308c6ab
Nolan Rangel <nrangel@sudscreative.com>
2024-03-04 19:16:06 -0700

    4.4 completed

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,6 +2,20 @@ const dummy = (blogs) => {
     return 1;
 }
 
+const totalLikes = (blogs) => {
+    if (blogs.length === 1) {
+        return blogs[0].likes;
+    } else {
+        let totalBlogLikes = 0;
+
+        blogs.map(blog => {
+            totalBlogLikes += blog.likes;
+        })
+        return totalBlogLikes;
+    }
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
